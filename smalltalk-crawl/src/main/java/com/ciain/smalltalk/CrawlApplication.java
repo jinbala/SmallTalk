a276277453f82fbfa2f1c86be94c192d26bd1940
CiaJin <1454953271@qq.com>
2022-05-01 13:09:17 +0800

    公共模块增加

---
+++ smalltalk-crawl/src/main/java/com/ciain/smalltalk/CrawlApplication.java
@@ -0,0 +1,15 @@
+package com.ciain.smalltalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jibaba
+ */
+@SpringBootApplication
+public class CrawlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlApplication.class);
+    }
+}
+
---
+++ smalltalk-crawl/src/main/java/com/ciain/smalltalk/mapper/CrawlBookIndexMapper.java
@@ -0,0 +1,16 @@
+package com.ciain.smalltalk.mapper;
+
+import com.java2nb.novel.entity.BookIndex;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author Administrator
+ */
+public interface CrawlBookIndexMapper extends BookIndexMapper {
+
+
+    /**
+     * 查询最后的章节
+     * */
+    BookIndex queryLastIndex(@Param("bookId") Long bookId);
+}
